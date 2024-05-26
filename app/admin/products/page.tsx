71c0dd6b575b954cbfc77ec69d5854caacdf3bcd
Milad Joodi <winbeta.com@gmail.com>
2024-05-27 02:34:45 +0330

    up

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+
 const page = () => {
     return (
-        <div>
-            products
-        </div>
+        <div className="m-10">
+      <h2 className="flex items-center justify-center font-semibold mb-2">
+        Products
+      </h2>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
     );
 }
 
